064deaac593bc73ee0ba3fd535554d07e2f5d158
Akmal Fahrurizal <akmal.fahrurizal@gmail.com>
2016-11-22 23:30:05 +0700

    ready to deploy

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+	if(req.header['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public'));
-app.listen(3000, function() {
-	console.log('Express server is running');
+
+app.listen(PORT, function() {
+	console.log('Express server is running port' + PORT);
 });
